57d69c521a44b132506c9184c27147269b8fe7d2
westwoong <westwoong>
2023-05-02 19:22:55 +0900

    Feat : sequelize FK NOT NULL 설정 추가

--- models/Index.js
+++ models/Index.js
@@ -2,15 +2,16 @@ const User = require('./Users');
 const Post = require('./Posts');
 const Photo = require('./Photos');
 const Comment = require('./Comments');
+const NullFalse = { foreignKey: { allowNull: false } };
 
-User.hasMany(Post);
-Post.belongsTo(User);
-Post.hasMany(Comment);
-User.hasMany(Comment);
-Post.hasMany(Photo);
-Photo.belongsTo(Post);
-Comment.belongsTo(Post);
-Comment.belongsTo(User);
+User.hasMany(Post, NullFalse);;
+Post.belongsTo(User, NullFalse);
+Post.hasMany(Comment, NullFalse);
+User.hasMany(Comment, NullFalse);
+Post.hasMany(Photo, NullFalse);
+Photo.belongsTo(Post, NullFalse);
+Comment.belongsTo(Post, NullFalse);
+Comment.belongsTo(User, NullFalse);
 
 module.exports = {
     User,
