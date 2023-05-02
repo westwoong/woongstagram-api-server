d1709ec3f6222fb3d42dd8a252bf38e7e1d25ec7
westwoong <westwoong>
2023-05-02 18:13:18 +0900

    Feat : 게시글 작성기능 추가

--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,16 @@ const express = require('express');
 const postsRoute = express.Router();
 
 postsRoute.post('/', async (req, res) => {
-    const { title, content } = req.body;
-    console.log(title, content);
-    res.status(201).json({ title, content });
-})
+    const { content } = req.body;
+    try {
+        const createPost = await Post.create({ content });
+        res.status(201).json(createPost);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('알 수 없는 오류가 발생하였습니다 관리자에게 문의 바랍니다.')
+    }
+});
 
 
 module.exports = postsRoute;
