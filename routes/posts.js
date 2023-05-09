dba4cae001c00e8e4bdde75ea4b4a08b0914245e
westwoong <westwoong>
2023-05-09 11:06:54 +0900

    Feat : JWT 인증&인가 추가, 이미지 용량 검증 추가

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,8 @@ const { Post, Photo } = require('../models');
 const express = require('express');
 const postsRoute = express.Router();
 const multer = require('multer');
+const jwt = require('jsonwebtoken');
+require('dotenv').config('../.env');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -12,33 +14,39 @@ const storage = multer.diskStorage({
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    const allowTypes = /jpg|png|gif/;
-    const filename = file.originalname;
-    console.log(filename);
-    const checkTypes = filename.slice(-3);
-    console.log(checkTypes);
-    const validTypes = allowTypes.test(checkTypes);
-    console.log(validTypes);
-    if (!validTypes) {
-        return cb(new Error('파일의 확장자는 jpg, png, gif 만 가능합니다'));
+const fileFilter = (req, file, cb, err) => {
+    try {
+        const allowTypes = /jpg|png|gif/;
+        const filename = file.originalname;
+        const checkTypes = filename.slice(-3);
+        const validTypes = allowTypes.test(checkTypes);
+        if (!validTypes) {
+            return cb(new Error('파일의 확장자는 jpg, png, gif 만 가능합니다'), false);
+        }
+        const maxFileSize = 20000000; // 20MB
+        if (file.size > maxFileSize) {
+            cb(new Error('사진이 너무커요'), false);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
     }
     cb(null, true);
 }
 
 const upload = multer({
     storage,
+    limits: { fileSize: 20000000 }, // byte 단위, 20Mb
     fileFilter
-});
+}).array('photos', 10);
 
 
 
 
-postsRoute.post('/', upload.array('photos'), async (req, res) => {
+postsRoute.post('/', upload, async (req, res, next) => {
     const { content } = req.body;
-    console.log(content);
     const photos = req.files;
-    console.log(photos)
+
     if (!content || content.length === 0) {
         return res.status(400).send('본문의 내용을 입력해 주시기 바랍니다.');
     }
@@ -51,8 +59,35 @@ postsRoute.post('/', upload.array('photos'), async (req, res) => {
     if (photos.length > 10) {
         return res.status(400).send('사진은 최대 10장 까지 첨부가 가능합니다.');
     }
+
+    const authHeader = req.headers.authorization;
+    console.log(req.headers.authorization);
+    if (authHeader === undefined) {
+        return res.status(400).send('로그인을 해주시기 바랍니다.');
+    }
+    let token = authHeader.substring(7, authHeader.length); // Bearer 뒤에 process.env.JSON_SECRETKEY 값 가져오는 substring메소드
+    let payload;
+
     try {
-        const createPost = await Post.create({ content, userId: 1 })
+        payload = jwt.verify(token, process.env.JSON_SECRETKEY);
+    } catch (err) {
+        if (err.message === "invalid signature") {
+            console.log(err.message);
+            return res.status(401).send("잘못된 토큰입니다.");
+        }
+        if (err.message === "jwt expired") {
+            console.log(err.message);
+            return res.status(401).send("유효기간이 만료된 토큰입니다.");
+        }
+        else {
+            console.log(err);
+            return res.status(500).send("로그인에러가 발생하였습니다 다시 로그인해주시기 바랍니다.");
+        }
+    }
+    const payloadArray = payload.id[0]; // payload.id 객체에 있는 id값만 받아오는 변수
+    console.log(payloadArray.id);
+    try {
+        const createPost = await Post.create({ content, userId: payloadArray.id });
         for (let PhotoArrayLength = 0; PhotoArrayLength < photos.length; PhotoArrayLength++) {
             await Photo.create({ url: photos[PhotoArrayLength].filename, sequence: PhotoArrayLength, postId: createPost.id });
         }
