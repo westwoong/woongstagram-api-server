749febf65c7d3d8707818696e5fd566760d20a32
westwoong <westwoong>
2023-05-14 19:33:18 +0900

    Feat : 게시물 작성 API 사진 URL 검증 추가 photos를 req.body로받아 url을 입력값으로 받고 해당 이미지 URL이 DB에 없을시 없는 배열 인덱스 404 return

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,44 @@
-const { Post } = require('../models');
+const { Post, Photo } = require('../models');
 const express = require('express');
 const postsRoute = express.Router();
-const multer = require('multer');
 const Authorization = require('../middleware/jsontoken');
 
 postsRoute.post('/', Authorization, async (req, res, next) => {
-    const { content } = req.body;
+    const { content, photos } = req.body;
     if (!content || content.length === 0) {
         return res.status(400).send('본문의 내용을 입력해 주시기 바랍니다.');
     }
     if (content.length > 1000) {
         return res.status(400).send('본문의 내용은 최대 1000자 까지 입력이 가능합니다.');
     }
-    if (!photos) {
-        return res.status(400).send('사진은 반드시 1장 이상 첨부해 주시기 바랍니다.');
-    }
-    if (photos.length > 10) {
-        return res.status(400).send('사진은 최대 10장 까지 첨부가 가능합니다.');
+    for (let PhotoArrayLength = 0; PhotoArrayLength < photos.length; PhotoArrayLength++) {
+        if (!photos[PhotoArrayLength].startsWith("http://localhost:3000/images/")) {
+            return res.status(400).send('http://localhost:3000/images/ 로 시작하는 이미지 주소를 입력해주세요');
+        }
     }
 
     const payloadArray = req.user[0];
     console.log(payloadArray.id);
+
     try {
+
+        for (let PhotoArrayLength = 0; PhotoArrayLength < photos.length; PhotoArrayLength++) {
+            const checkPhotoUrl = await Photo.findAll({ attributes: ['url'], where: { url: photos[PhotoArrayLength] } });
+            console.log('--------1---------');
+            console.log(checkPhotoUrl);
+            console.log('--------1---------');
+            if (checkPhotoUrl.length === 0) {  // 이미지가 없을시? 배열의 값이 [] 로 출력되어 길이로 변경
+                return res.status(404).send(`${photos[PhotoArrayLength]}해당 이미지는 존재하지 않습니다.`);
+            }
+        }
+        //  else {
         const createPost = await Post.create({ content, userId: payloadArray.id });
-        res.status(201).json({ createPost });
+        return res.status(201).json({ createPost });
+        // }
 
     } catch (err) {
         console.error(err);
-        res.status(500).send('알 수 없는 오류가 발생하였습니다 관리자에게 문의 바랍니다.')
+        return res.status(500).send('알 수 없는 오류가 발생하였습니다 관리자에게 문의 바랍니다.')
     }
 });
 
