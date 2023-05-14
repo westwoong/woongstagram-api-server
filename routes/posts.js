c14885b760710e719955c2fc2a40d45ffa954775
westwoong <westwoong>
2023-05-15 01:49:38 +0900

    Feat : 게시물 작성 API 완료

--- routes/posts.js
+++ routes/posts.js
@@ -23,18 +23,32 @@ postsRoute.post('/', Authorization, async (req, res, next) => {
     const t = await transaction;
 
     try {
-        const test123 = await sequelize.transaction(async (t) => {
+        // sequelize 트랜잭션 매서드
+        const postTransaction = await sequelize.transaction(async (t) => {
             const createPost = await Post.create({ content, userId: payloadArray.id }, { transaction: t });
-            for (let PhotoArrayLength = 0; PhotoArrayLength < photos.length; PhotoArrayLength++) {
-                const checkPhotoUrl = await Photo.findAll({ attributes: ['url'], where: { url: photos[PhotoArrayLength] } });
-                if (checkPhotoUrl.length === 0) {  // 이미지가 없을시? 배열의 값이 [] 로 출력되어 길이로 변경
-                    return res.status(404).send(`${photos[PhotoArrayLength]}해당 이미지는 존재하지 않습니다.`);
+
+            const photoPromise = photos.map(async (photosUrl) => {
+                const checkPhotoUrl = await Photo.findOne({ where: { url: photosUrl } }); // map array에 담겨있는 URL를 찾는 변수
+                console.log('----------------------')
+                console.log(checkPhotoUrl);
+                console.log('----------------------')
+                if (!checkPhotoUrl) {
+                    // map array에 없는게 있을경우 캐치하여 해당 배열 URL반환
+                    return res.status(404).send(`${photosUrl}해당 이미지는 존재하지 않습니다.`);
                 }
-                await Photo.update({ postId: createPost.id }, { where: { url: photos[PhotoArrayLength] } }, { transaction: t });
-            }
+                /*
+                찾은 URL의 같은필드에있는 post_id를 게시물의 PK(createPost.id)로 update시켜준다
+                이때 { transaction: t } 으로 묶은 게시물작성과, 사진 FK 업데이트중 하나라도 실패한다면 쿼리 롤백
+                */
+                await checkPhotoUrl.update({ postId: createPost.id }, { transaction: t });
+
+            });
+            // photoPromise 함수의 모든 작업이 완료될때까지 대기한다.
+            // 모든 작업을 동시에 실행하여 하나라도 거부될경우 오류를 반환
+            await Promise.all(photoPromise);
             return createPost;
         });
-        return res.status(201).json({ post: test123 });
+        return res.status(201).send({ postTransaction });
     } catch (err) {
         console.error(err);
         return res.status(500).send('알 수 없는 오류가 발생하였습니다 관리자에게 문의 바랍니다.')
