364c619f1aaec6ff11a341ee9ca038daa05245bd
westwoong <westwoong>
2023-05-14 23:09:52 +0900

    Feat : DB 모듈 비구조화 할당 적용

--- models/Posts.js
+++ models/Posts.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database');
 
 const Post = sequelize.define('posts', {
     id: {
