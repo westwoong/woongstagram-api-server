949298b0dbd01e9a458f53a84b52bcd0eb513e2e
westwoong <westwoong>
2023-05-21 18:27:59 +0900

    Feat : 게시글 좋아요 기능 추가

--- routes/likes.js
+++ routes/likes.js
@@ -2,10 +2,13 @@ const { Like } = require('../models');
 const express = require('express');
 const likesRoute = express.Router();
 const Authorization = require('../middleware/jsontoken');
+const ErrorCatch = require('../middleware/trycatch');
 
-likesRoute.post('/:postId', Authorization, async (req, res) => {
+likesRoute.post('/:postId', Authorization, ErrorCatch(async (req, res) => {
+    const { postId } = req.params;
     const payloadArray = req.user[0];
-
-})
+    await Like.create({ userId: payloadArray.id, postId });
+    res.status(204).send();
+}));
 
 module.exports = likesRoute;
