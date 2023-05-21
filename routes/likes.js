608708b62d2328ea094154d7ae45e559b937d47b
westwoong <westwoong>
2023-05-21 19:16:11 +0900

    Style : 좋아요 중복처리 상태코드 수정

--- routes/likes.js
+++ routes/likes.js
@@ -9,7 +9,7 @@ likesRoute.post('/:postId', Authorization, ErrorCatch(async (req, res) => {
     const payloadArray = req.user[0];
     const checkLikes = await Like.findOne({ where: { postId, userId: payloadArray.id } });
     if (checkLikes) {
-        return res.status(400).send('이미 좋아요를 누른 게시글입니다.');
+        return res.status(409).send('이미 좋아요를 누른 게시글입니다.');
     }
     await Like.create({ userId: payloadArray.id, postId });
     res.status(204).send();
