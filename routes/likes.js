8189891a8102efce0dc68d2706d9a641d7e90be4
westwoong <westwoong>
2023-05-21 19:14:03 +0900

    Feat : 게시글  좋아요 중복 확인부분 추가

--- routes/likes.js
+++ routes/likes.js
@@ -7,6 +7,17 @@ const ErrorCatch = require('../middleware/trycatch');
 likesRoute.post('/:postId', Authorization, ErrorCatch(async (req, res) => {
     const { postId } = req.params;
     const payloadArray = req.user[0];
+    const checkLikes = await Like.findOne({ where: { postId, userId: payloadArray.id } });
+    if (checkLikes) {
+        return res.status(400).send('이미 좋아요를 누른 게시글입니다.');
+    }
+    await Like.create({ userId: payloadArray.id, postId });
+    res.status(204).send();
+}));
+
+likesRoute.delete('/:postId', Authorization, ErrorCatch(async (req, res) => {
+    const { postId } = req.params;
+    const payloadArray = req.user[0];
     await Like.create({ userId: payloadArray.id, postId });
     res.status(204).send();
 }));
