d5b2a8d4262e1ed436b6e86e15a6d0e76ee127e9
westwoong <westwoong>
2023-05-24 16:47:37 +0900

    Refactor : 댓글 수정&삭제 기능 리팩토링

--- routes/comments.js
+++ routes/comments.js
@@ -11,50 +11,50 @@ commentsRoute.post('/:postId', Authorization, ErrorCatch(async (req, res, next) 
     if (!comment || comment.length > 100) {
         return res.status(400).send('댓글의 내용은 1글자 이상 100글자 이하로 작성이 가능합니다');
     }
-    const payloadArray = req.user[0];
+    const userId = req.user[0].id;
 
-    await Comment.create({ userId: payloadArray.id, postId, content: comment });
+    await Comment.create({ userId, postId, content: comment });
     return res.status(201).send('작성이 완료되었습니다');
 }));
 
-commentsRoute.patch('/:postId/:commentId', Authorization, ErrorCatch(async (req, res, next) => {
-    const { postId, commentId } = req.params;
+commentsRoute.patch('/:commentId', Authorization, ErrorCatch(async (req, res, next) => {
+    const { commentId } = req.params;
     const { comment } = req.body;
-    const payloadArray = req.user[0];
-    const usercheck = await Comment.findOne({ where: { id: commentId, userId: payloadArray.id, postId } });
+    const userId = req.user[0].id;
+    const foundComment = await Comment.findOne({ where: { id: commentId } });
+
+    if (foundComment?.userId !== userId || foundComment === nul) {
+        return res.status(403).send('본인의 댓글만 수정이 가능합니다');
+    }
+
+    if (!foundComment) {
+        return res.status(400).send('수정하려는 댓글이 존재하지 않습니다.');
+    }
 
     if (!comment || comment.length > 100) {
         return res.status(400).send('댓글의 내용은 1글자 이상 100글자 이하로 작성이 가능합니다');
     }
-    if (!usercheck) {
-        return res.status(403).send('권한이 없습니다');
-    }
-    if (usercheck) {
-        if (usercheck.userId && usercheck.userId !== payloadArray.id) {
-            return res.status(403).send('본인의 댓글만 수정이 가능합니다');
-        }
-    }
 
-    await Comment.update({ content: comment }, { where: { id: commentId, userId: payloadArray.id, postId } });
-    return res.status(201).send('수정이 완료되었습니다');
+    await Comment.update({ content: comment }, { where: { id: commentId, userId } });
+    return res.status(204).send();
+
 }));
 
-commentsRoute.delete('/:postId/:commentId', Authorization, ErrorCatch(async (req, res, next) => {
-    const { postId, commentId } = req.params;
-    const payloadArray = req.user[0];
+commentsRoute.delete('/:commentId', Authorization, ErrorCatch(async (req, res, next) => {
+    const { commentId } = req.params;
+    const userId = req.user[0].id;
 
-    const usercheck = await Comment.findOne({ where: { id: commentId, userId: payloadArray.id, postId } });
+    const foundComment = await Comment.findOne({ where: { id: commentId, userId } });
 
-    if (!usercheck) {
-        return res.status(403).send('권한이 없습니다');
-    }
-    if (usercheck) {
-        if (usercheck.userId && usercheck.userId !== payloadArray.id) {
-            return res.status(403).send('본인의 댓글만 수정이 가능합니다');
-        }
+    if (foundComment?.userId !== userId || foundComment === null) {
+        return res.status(403).send('본인의 댓글만 삭제가 가능합니다');
     }
 
-    await Comment.destroy({ where: { id: commentId, userId: payloadArray.id, postId } });
+    if (!foundComment) {
+        return res.status(400).send('삭제하려는 댓글이 존재하지 않습니다.');
+    }
+
+    await Comment.destroy({ where: { id: commentId, userId } });
     return res.status(201).send('댓글 삭제가 완료되었습니다');
 }));
 
