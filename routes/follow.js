80fe1d1d1ffde7342b5ae303b739d5fa17bb76b2
westwoong <westwoong>
2023-05-26 12:02:24 +0900

    Feat : 팔로우 삭제 기능 추가

--- routes/follow.js
+++ routes/follow.js
@@ -8,12 +8,25 @@ const ErrorCatch = require('../middleware/trycatch');
 followsRoute.post('/:followId', Authorization, ErrorCatch(async (req, res, next) => {
     const { followId } = req.params;
     const followerId = req.user[0].id;
+    console.log('aaaaa');
+    console.log(followId);
+    console.log('aaaaa');
+    console.log('bbbb');
+    console.log(followerId)
+    console.log('bbbb');
+    if (followId == followerId) {
+        return res.status(400).send('본인을 팔로우 할 순 없습니다 ㅎㅎ...');
+    }
+    await Follower.create({ followId, followerId });
+    return res.status(204).send();
+}));
 
-    const testresult = await Follower.create({ followId, followerId });
-    console.log(testresult);
+followsRoute.delete('/:followId', Authorization, ErrorCatch(async (req, res, next) => {
+    const { followId } = req.params;
+    const followerId = req.user[0].id;
 
+    await Follower.destroy({ where: { followerId, followId } })
     return res.status(204).send();
 }));
 
 module.exports = followsRoute;
-//followerId
