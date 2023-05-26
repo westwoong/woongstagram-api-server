14a5b28ec2f1cba71bfbfb93120cb45b46aa4840
westwoong <westwoong>
2023-05-26 12:02:52 +0900

    Feat : 팔로우 유효성 검증 추가

--- routes/follow.js
+++ routes/follow.js
@@ -8,15 +8,11 @@ const ErrorCatch = require('../middleware/trycatch');
 followsRoute.post('/:followId', Authorization, ErrorCatch(async (req, res, next) => {
     const { followId } = req.params;
     const followerId = req.user[0].id;
-    console.log('aaaaa');
-    console.log(followId);
-    console.log('aaaaa');
-    console.log('bbbb');
-    console.log(followerId)
-    console.log('bbbb');
+
     if (followId == followerId) {
         return res.status(400).send('본인을 팔로우 할 순 없습니다 ㅎㅎ...');
     }
+
     await Follower.create({ followId, followerId });
     return res.status(204).send();
 }));
