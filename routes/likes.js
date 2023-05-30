e42f25c9a07d4e3b1bb5bdd0a2b98b7f36d0b465
westwoong <westwoong>
2023-05-31 00:10:47 +0900

    Feat : 게시글 좋아요 API 예외처리 추가

--- routes/likes.js
+++ routes/likes.js
@@ -1,4 +1,4 @@
-const { Like } = require('../models');
+const { Like, Post } = require('../models');
 const express = require('express');
 const likesRoute = express.Router();
 const Authorization = require('../middleware/jsontoken');
@@ -6,19 +6,24 @@ const ErrorCatch = require('../middleware/trycatch');
 
 likesRoute.post('/:postId', Authorization, ErrorCatch(async (req, res) => {
     const { postId } = req.params;
-    const payloadArray = req.user[0];
-    const checkLikes = await Like.findOne({ where: { postId, userId: payloadArray.id } });
+    const userId = req.user[0].id;
+    const checkLikes = await Like.findOne({ where: { postId, userId } });
+    const checkExistPost = await Post.findOne({ where: { id: postId } });
+    if (!checkExistPost) {
+        return res.status(404).send('없는 게시물 입니다.');
+    }
     if (checkLikes) {
         return res.status(409).send('이미 좋아요를 누른 게시글입니다.');
     }
-    await Like.create({ userId: payloadArray.id, postId });
+
+    await Like.create({ userId, postId });
     res.status(204).send();
 }));
 
 likesRoute.delete('/:postId', Authorization, ErrorCatch(async (req, res) => {
     const { postId } = req.params;
-    const payloadArray = req.user[0];
-    await Like.destroy({ where: { userId: payloadArray.id, postId } });
+    const userId = req.user[0].id;
+    await Like.destroy({ where: { userId, postId } });
     res.status(204).send();
 }));
 
