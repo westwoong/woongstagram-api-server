225947a4b62b43e5a559775c58bb84c5ca5e88e8
westwoong <westwoong>
2023-06-21 02:13:31 +0900

    Remove : 409 에러처리클래스 중복파일 삭제

--- errors/ ConflictException.js
+++
@@ -1,8 +0,0 @@
-const HttpException = require('./HttpException');
-class ConflictException extends HttpException {
-  constructor(message) {
-    super(message, 409);
-  }
-}
-
-module.exports = ConflictException;
