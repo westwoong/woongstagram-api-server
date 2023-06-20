9426d57e55a539211b71e9dbc0b5daa749ec3a12
westwoong <westwoong>
2023-06-20 18:54:15 +0900

    Feat : HTTP 409 에러처리 클래스 추가

--- errors/IndexException.js
+++ errors/IndexException.js
@@ -2,10 +2,12 @@ const BadRequestException = require('./BadRequestException');
 const NotFoundException = require('./NotFoundException');
 const ForbiddenException = require('./ForbiddenException');
 const UnauthorizedException = require('./UnauthorizedException');
+const ConflictException = require('./UnauthorizedException');
 
 module.exports = {
   BadRequestException,
   UnauthorizedException,
   ForbiddenException,
-  NotFoundException
+  NotFoundException,
+  ConflictException
 };
