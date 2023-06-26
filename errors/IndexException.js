ffb6a7686f6b2ed7aa3baebada45e637aa11ef37
westwoong <westwoong>
2023-06-26 16:21:09 +0900

    Feat : 409 에러처리 index require 오타 수정

--- errors/IndexException.js
+++ errors/IndexException.js
@@ -2,7 +2,7 @@ const BadRequestException = require('./BadRequestException');
 const NotFoundException = require('./NotFoundException');
 const ForbiddenException = require('./ForbiddenException');
 const UnauthorizedException = require('./UnauthorizedException');
-const ConflictException = require('./UnauthorizedException');
+const ConflictException = require('./ConflictException');
 
 module.exports = {
   BadRequestException,
