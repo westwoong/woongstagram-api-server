aba8681b54ad9691c280109a5663855b7e807711
westwoong <westwoong>
2023-06-26 16:43:53 +0900

    Feat : 팔로우 API 유효성검증 추가

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,10 +1,15 @@
 const asyncHandler = require('../middleware/asyncHandler');
-const { BadRequestException, ConflictException } = require('../errors/IndexException');
+const { BadRequestException, ConflictException, NotFoundException } = require('../errors/IndexException');
 const { followByUserId, unFollowByUserId, isDuplicateFollow } = require('../repository/followRepository');
+const { isExistByUserId } = require('../repository/userRepository');
 
-module.exports.follow = asyncHandler(async (req, res, next) => {
+module.exports.follow = asyncHandler(async (req, res) => {
   const { followId } = req.params;
   const followerId = req.user[0].id;
+
+  if (!isExistByUserId(followId)) {
+    throw new NotFoundException('해당 사용자는 존재하지 않습니다');
+  }
 
   if (followId == followerId) {
     throw new BadRequestException('본인을 팔로우 할 순 없습니다.');
@@ -18,9 +23,17 @@ module.exports.follow = asyncHandler(async (req, res, next) => {
   return res.status(204).send();
 })
 
-module.exports.unfollow = asyncHandler(async (req, res, next) => {
+module.exports.unfollow = asyncHandler(async (req, res) => {
   const { followId } = req.params;
   const followerId = req.user[0].id;
+
+  if (!isExistByUserId(followId)) {
+    throw new NotFoundException('해당 사용자는 존재하지 않습니다');
+  }
+
+  if (followId == followerId) {
+    throw new BadRequestException('본인을 제외한 사용자에만 가능합니다');
+  }
 
   if (!await isDuplicateFollow(followerId, followId)) {
     throw new ConflictException('이미 언팔로우한 사용자 입니다.');
--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,8 +1,14 @@
 const { User } = require('../models');
+
 const findOneUserNicknameAndNameByUserId = async (userId) => {
   return User.findOne({ where: { id: userId }, attributes: ['nickname', 'name'] });
 }
 
+const isExistByUserId = async (userId) => {
+  return User.findAll({ where: { id: userId } });
+}
+
 module.exports = {
-  findOneUserNicknameAndNameByUserId
+  findOneUserNicknameAndNameByUserId,
+  isExistByUserId
 }
