d011433c5c5c0ebcb7e48244560f221a6d13f674
westwoong <westwoong>
2023-06-26 16:29:22 +0900

    Feat : 팔로우 API 유효성검증 에러코드 수정

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('../middleware/asyncHandler');
-const { BadRequestException } = require('../errors/IndexException');
+const { BadRequestException, ConflictException } = require('../errors/IndexException');
 const { followByUserId, unFollowByUserId, isDuplicateFollow } = require('../repository/followRepository');
 
 module.exports.follow = asyncHandler(async (req, res, next) => {
@@ -11,7 +11,7 @@ module.exports.follow = asyncHandler(async (req, res, next) => {
   }
 
   if (await isDuplicateFollow(followerId, followId)) {
-    throw new BadRequestException('중복적으로 팔로우할 수 없습니다.');
+    throw new ConflictException('중복적으로 팔로우할 수 없습니다.');
   }
 
   await followByUserId(followId, followerId);
@@ -23,7 +23,7 @@ module.exports.unfollow = asyncHandler(async (req, res, next) => {
   const followerId = req.user[0].id;
 
   if (!await isDuplicateFollow(followerId, followId)) {
-    throw new BadRequestException('이미 언팔로우한 사용자 입니다.');
+    throw new ConflictException('이미 언팔로우한 사용자 입니다.');
   }
 
   await unFollowByUserId(followerId, followId)
