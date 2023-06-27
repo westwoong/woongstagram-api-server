a9909fa5e9bf5c9cb9f84288f6807ccdd6be1a53
westwoong <westwoong>
2023-06-28 05:40:48 +0900

    Refactor : 사진 업로드 유효성 검증 분리

--- routes/upload.js
+++ routes/upload.js
@@ -2,10 +2,10 @@ const express = require('express');
 const uploadRoute = express.Router();
 const authorization = require('../middleware/jwtAuth');
 const uploadController = require('../controllers/uploadController');
-const uploadService = require('../service/uploadService');
+const uploadValidator = require('../service/validators/uploadValidator');
 
-uploadRoute.post('/', authorization, uploadService.module, uploadController.upload);
+uploadRoute.post('/', authorization, uploadValidator.module, uploadController.upload);
 
-uploadRoute.patch('/:photoId', authorization, uploadService.module, uploadController.modifyPhotos);
+uploadRoute.patch('/:photoId', authorization, uploadValidator.module, uploadController.modifyPhotos);
 
 module.exports = uploadRoute;
