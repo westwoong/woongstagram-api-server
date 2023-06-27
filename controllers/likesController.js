6d4e2fae22a129e57d71423a570b9e71b3c26650
westwoong <westwoong>
2023-06-27 23:21:05 +0900

    Refactor : 좋아요 정보 검색 API service 분리

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -38,42 +38,11 @@ module.exports.unlikeIt = asyncHandler(async (req, res) => {
 
 module.exports.search = asyncHandler(async (req, res) => {
     const { postId } = req.params;
-    const page = req.query.page || 1;
-    const limit = 2;
-    const offset = (page - 1) * limit;
 
-    const likes = await getLikedByPostId(postId, limit, offset);
-
-    const likesData = [];
-
-    for (const like of likes) {
-        const users = await getUserInfoByUserId(like.userId, limit, offset);
-        const posts = await getInfoByPostId(postId, limit, offset);
-        const follows = await isFollowingByUserId(posts.userId);
-        const followCheck = follows ? true : false;
-
-        likesData.push({
-            name: users.name,
-            nickname: users.nickname,
-            isFollower: followCheck
-        })
+    if (!postId) {
+        throw new BadRequestException('postId 값이 존재하지 않습니다.');
     }
 
-    const totalLikesCount = await getLikeCountByPostId(postId);
-    const totalPages = Math.ceil(totalLikesCount / limit);
-    const hasNextPage = page < totalPages;
-    const hasPreviousPage = page > 1;
-
-    return res.status(200).send({
-        likesData: likesData,
-        pagination: {
-            page,
-            limit,
-            totalLikesCount,
-            totalPages,
-            hasNextPage,
-            hasPreviousPage
-        }
-
-    });
+    const result = await likeService.searchPostLikedUsersInfo(postId, req);
+    return res.status(200).send(result);
 });
--- service/likeService.js
+++ service/likeService.js
@@ -25,3 +25,43 @@ module.exports.unlike = async (postId, userId) => {
 
     return unLikeByPostIdAndUserId(postId, userId);
 }
+
+module.exports.searchPostLikedUsersInfo = async (postId, req) => {
+    const page = req.query.page || 1;
+    const limit = 20;
+    const offset = (page - 1) * limit;
+
+    const likes = await getLikedByPostId(postId, limit, offset);
+
+    const likesData = [];
+
+    for (const like of likes) {
+        const users = await getUserInfoByUserId(like.userId, limit, offset);
+        const posts = await getInfoByPostId(postId, limit, offset);
+        const follows = await isFollowingByUserId(posts.userId);
+        const followCheck = follows ? true : false;
+
+        likesData.push({
+            name: users[0].dataValues.name,
+            nickname: users[0].dataValues.nickname,
+            isFollower: followCheck
+        })
+    }
+
+    const totalLikesCount = await getLikeCountByPostId(postId);
+    const totalPages = Math.ceil(totalLikesCount / limit);
+    const hasNextPage = page < totalPages;
+    const hasPreviousPage = page > 1;
+
+    return {
+        likesData,
+        pagination: {
+            page,
+            limit,
+            totalLikesCount,
+            totalPages,
+            hasNextPage,
+            hasPreviousPage
+        }
+    }
+}
