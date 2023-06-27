8bafcfa14de9c1d351ca5d7248467d8da6eeef97
westwoong <westwoong>
2023-06-28 02:57:46 +0900

    Refactor : 파일 업로드 API service 분리

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,61 +1,18 @@
-const { S3Client } = require('@aws-sdk/client-s3');
 const { BadRequestException } = require('../errors/IndexException');
 const { createPhotos, modifyPhotosByPostId } = require('../repository/photoRepository');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
 const asyncHandler = require('../middleware/asyncHandler');
 require('dotenv').config('../.env');
-
-const fileFilter = (req, file, cb) => {
-    const photos = req.files;
-    if (!photos) {
-        throw new BadRequestException('사진은 반드시 1장 이상 첨부해 주시기 바랍니다.');
-    }
-    if (photos.length > 10) {
-        throw new BadRequestException('사진은 최대 10장 까지 첨부가 가능합니다.');
-    }
-    if (!file.mimetype == 'image/jpg' || !file.mimetype == 'image/jpeg' || !file.mimetype == 'image/png' || !file.mimetype == 'image/gif') {
-        return cb(new BadRequestException('파일의 확장자는 jpg, png, gif 만 가능합니다'), false);
-    }
-    const maxFileSize = 20000000; // 20MB
-    if (file.size > maxFileSize) {
-        return cb(new BadRequestException('파일의 용량은 20mb까지 업로드가 가능합니다.'), false);
-    } else {
-        cb(null, true);
-    }
-}
-
-const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY,
-        secretAccessKey: process.env.S3_SECRET_KEY
-    }
-})
-
-module.exports.module = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'woong-nodejs-uploaded-files',
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
-        key: function (req, file, cb) {
-            cb(null, Date.now() + file.originalname);
-        }
-    }),
-    limits: { fileSize: 20000000 }, // byte 단위, 20Mb
-    fileFilter
-}).array('photos', 10);
+const uploadService = require('../service/uploadService');
 
 module.exports.upload = asyncHandler(async (req, res) => {
     const photos = req.files;
 
-    for (let PhotoArrayLength = 0; PhotoArrayLength < photos.length; PhotoArrayLength++) {
-        const { location } = photos[PhotoArrayLength];
-        await createPhotos(location, PhotoArrayLength);
+    if (!photos) {
+        throw new BadRequestException('photos 값이 존재하지 않습니다.');
     }
-    return res.status(201).json(photos);
+
+    const result = await uploadService.upload(photos);
+    return res.status(201).json({ iamgeUrl: result });
 });
 
 module.exports.modifyPhotos = asyncHandler(async (req, res) => {
