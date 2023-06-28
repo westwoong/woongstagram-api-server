c1161ad262ebbe11d724e96d589d35fa165054de
westwoong <westwoong>
2023-06-28 16:41:26 +0900

    Feat : asyncHandler 개발환경 분리

--- middleware/asyncHandler.js
+++ middleware/asyncHandler.js
@@ -1,8 +1,12 @@
+require('dotenv').config('../.env');
 module.exports = (requestHandler) => {
     return async (req, res, next) => {
         try {
             await requestHandler(req, res);
         } catch (err) {
+            if (process.env.SERVER_SETTING == 'DEV') {
+                console.log(err);
+            }
             next(err);
         }
     }
