2cf539268e87990e4d3fed3ceb96f2190e1800b4
westwoong <westwoong>
2023-07-04 13:34:39 +0900

    Refactor : 게시글 작성 유효성검증 함수 분리

--- service/validators/postValidator.js
+++ service/validators/postValidator.js
@@ -2,12 +2,20 @@ const { BadRequestException } = require('../../errors/IndexException');
 require('dotenv').config('../../.env');
 
 module.exports.validatePost = (content, photos) => {
+  validateContent(content);
+  validateContentPhotos(photos);
+}
+
+function validateContent(content) {
   if (!content || content.length === 0) {
     throw new BadRequestException('본문의 내용을 입력해 주시기 바랍니다');
   }
   if (content.length > 1000) {
     throw new BadRequestException('본문의 내용은 최대 1000자 까지 입력이 가능합니다.');
   }
+}
+
+function validateContentPhotos(photos) {
   for (let photoArrayLength = 0; photoArrayLength < photos.length; photoArrayLength++) {
     if (!photos[photoArrayLength].startsWith(`${process.env.S3_BUCKET_URL}`)) {
       throw new BadRequestException(`${process.env.S3_BUCKET_URL} 로 시작하는 이미지 주소를 입력해주세요`);
