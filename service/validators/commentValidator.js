7aab45bb7b77178210ae1911204e5c3870d2827a
westwoong <westwoong>
2023-07-13 00:12:34 +0900

    Feat : 댓글 작성 API 함수 불필요한 검증 제거

--- service/validators/commentValidator.js
+++ service/validators/commentValidator.js
@@ -5,9 +5,6 @@ module.exports.validateCreateComment = async (comment) => {
     if (!comment || comment.length > 100) {
         throw new BadRequestException('댓글은 1글자 이상 100글자 이하로 작성해야 합니다');
     }
-    if (!comment) {
-        throw new BadRequestException('삭제하려는 댓글이 존재하지 않습니다.');
-    }
 }
 
 module.exports.validateDeleteAndModifyComment = async (commentId, userId) => {
